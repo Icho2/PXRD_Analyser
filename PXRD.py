68347f030acc93e23e50b8d496c9234bbf2610bb
Mauricio Agurcia <104690999+Icho2@users.noreply.github.com>
2023-07-24 11:44:13 -0500

    added dft

--- PXRD.py
+++ PXRD.py
@@ -1,34 +1,51 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Just replace the path for your .csv file's path
-path = input("Enter file path: ") #make sure to get rid of the quaotations in your input
+# Just replace the path for your .csv file's path
+path = input("Enter file path: ")  # make sure to get rid of the quaotations in your input
 include = input("Enter what graphs you want to see (for all type all): ")
+title = input("Enter title: ")
+color = input("Do you want to color code your graphs? (y/n) ")
 
 if (path == "example"):
-    path = "C:/Users/Mauricio/Desktop/MOFs/PXRD/Excel_files/ligand.csv"
+    path = 'C:/Users/mauri/OneDrive/Desktop/Humphrey_Lab/Ben Siu/ligand.csv'
+else:
+    path = path.replace('\\', '/')
+    path = path.replace('"', '')
+
+# hard-coded variables
 data = pd.read_csv(open(path), sep=',')
 x = data[data.columns[0]]
-start = 1
 translation = 0
+used_colors = {}
+
 if include == "all":
-    include = list(range(0,len(data.columns)+1))
+    graphs = list(range(0, len(data.columns) + 1))
 else:
     include = include.split()
     include = [eval(i) for i in include]
     graphs = []
-    start = 1
-    for i in range(1, len(include)+1):
-        graphs.append(start)
-        start += 2
-    print(graphs)
+    for i in include:
+        n = (i * 2) - 1
+        graphs.append(n)
+
 for i in graphs:
-    if (i % 2 != 0): # you can possibly erase this if statement
+    if color == 'n':
         col = data.columns[i]
         y = abs(min(data[col])) + data[col]
-        z = y/(max(y)) + translation
-        translation+=1.5
-        plt.plot(x, z, label=data.columns[i-1])
-plt.title("samples")
+        z = y / (max(y)) + translation
+        translation += 1.5
+        plt.plot(x, z, label=data.columns[i - 1])
+    elif color == 'y':
+        col = data.columns[i]
+        y = abs(min(data[col])) + data[col]
+        z = y / (max(y)) + translation
+        translation += 1.5
+        print("Used colors: ", used_colors)
+        paint = input("Enter color for graph " + data.columns[i - 1] + " : ")
+        used_colors[data.columns[i - 1]] = paint
+        plt.plot(x, z, label=data.columns[i - 1], c=paint)
+
+plt.title(title)
 plt.legend()
 plt.show()
