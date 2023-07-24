b5934f8802b4b1c6d479bfa291db54209b7551f2
Mauricio Agurcia <104690999+Icho2@users.noreply.github.com>
2023-07-23 23:44:53 -0500

    done :)

--- PXRD.py
+++ PXRD.py
@@ -9,15 +9,21 @@ if (path == "example"):
     path = "C:/Users/Mauricio/Desktop/MOFs/PXRD/Excel_files/ligand.csv"
 data = pd.read_csv(open(path), sep=',')
 x = data[data.columns[0]]
+start = 1
 translation = 0
 if include == "all":
     include = list(range(0,len(data.columns)+1))
 else:
     include = include.split()
-    #include = list(include, sep=' ')
-print(include)
-for i in include:
-    if (i % 2 != 0):
+    include = [eval(i) for i in include]
+    graphs = []
+    start = 1
+    for i in range(1, len(include)+1):
+        graphs.append(start)
+        start += 2
+    print(graphs)
+for i in graphs:
+    if (i % 2 != 0): # you can possibly erase this if statement
         col = data.columns[i]
         y = abs(min(data[col])) + data[col]
         z = y/(max(y)) + translation
